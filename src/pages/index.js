2e4b0edc3274aa2775ed060ab08dda66409cacb8
NRD89 <ndaltondev@gmail.com>
2020-03-04 09:26:31 -0800

    First commit. Added and implemented react-vector-maps.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,43 @@
 import React from "react"
-import { Link } from "gatsby"
+import { VectorMap } from "@south-paw/react-vector-maps"
+import styled from "styled-components"
 
+import usa from "../maps/usa.json"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+
+const Map = styled.div`
+  margin: 1rem auto;
+  width: 100%;
+
+  svg {
+    stroke: #fff;
+
+    // All layers are just path elements
+    path {
+      fill: rgb(73, 118, 56);
+      cursor: pointer;
+      outline: none;
+
+      // When a layer is hovered
+      &:hover {
+        fill: rgba(73, 118, 56, 0.83);
+      }
+
+      // When a layer is focused.
+      &:focus {
+        fill: rgba(73, 118, 56, 0.6);
+      }
+    }
+  }
+`
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Map>
+      <VectorMap {...usa} />
+    </Map>
   </Layout>
 )
 
