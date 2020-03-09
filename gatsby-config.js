103040317a9dcdca1c3ad9ac0c14bc86dfb1c838
NRD89 <ndaltondev@gmail.com>
2020-03-09 16:11:15 -0700

    added: THC logo as the favicon, updated meta description just as a placeholder for now

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `THC Solutions Cannabis Policy Page`,
+    description: `Find information on Cannabis policies within the United States with our interactive map!`,
+    author: `@`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `thc-us-cannabis-policy-page`,
+        short_name: `thc-us-cannabis-map`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#4a7729 `,
+        theme_color: `#4a7729 `,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/thc-logo@300x.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-styled-components`,
