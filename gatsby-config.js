2040d65cfe0c149ea5bb9c79ef1874a0b098fd5c
NRD89 <ndaltondev@gmail.com>
2020-06-09 16:01:00 -0700

    added: google analytics plugin and analytics tag to gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,6 +42,12 @@ module.exports = {
         watchMode: `true`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-83589888-1",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
